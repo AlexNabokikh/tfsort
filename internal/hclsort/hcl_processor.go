1c96e59486618870d1c5222bc99ee37704f53eef
Christopher Barnes <christopher@barnes.biz>
2025-05-21 14:20:17 +0200

    fix: lint

--- internal/hclsort/hcl_processor.go
+++ internal/hclsort/hcl_processor.go
@@ -32,42 +32,40 @@ func ParseHCLContent(
 // sortRequiredProvidersInBlock sorts the entries in any required_providers block
 // within a terraform block, alphabetically by provider name, preserving tokens.
 func sortRequiredProvidersInBlock(block *hclwrite.Block) {
-    for _, b := range block.Body().Blocks() {
-        if b.Type() != "required_providers" {
-            continue
-        }
-        body := b.Body()
-        attrs := body.Attributes()
+	for _, b := range block.Body().Blocks() {
+		if b.Type() != "required_providers" {
+			continue
+		}
+		body := b.Body()
+		attrs := body.Attributes()
 
-        providerNames := make([]string, 0, len(attrs))
-        for name := range attrs {
-            providerNames = append(providerNames, name)
-        }
-        sort.Strings(providerNames)
+		providerNames := make([]string, 0, len(attrs))
+		for name := range attrs {
+			providerNames = append(providerNames, name)
+		}
+		sort.Strings(providerNames)
 
-        body.Clear()
-        body.AppendNewline()
+		body.Clear()
+		body.AppendNewline()
 
-        for i, name := range providerNames {
-            attr := attrs[name]
-            tokens := attr.BuildTokens(nil)
+		for i, name := range providerNames {
+			attr := attrs[name]
+			tokens := attr.BuildTokens(nil)
 
-            start, end := 0, len(tokens)
-            for start < end && tokens[start].Type == hclsyntax.TokenNewline {
-                start++
-            }
-            for end > start && tokens[end-1].Type == hclsyntax.TokenNewline {
-                end--
-            }
-
-            body.AppendUnstructuredTokens(tokens[start:end])
-            if i+1 < len(providerNames) {
-                body.AppendNewline()
-            }
-        }
-
-        body.AppendNewline()
-    }
+			start, end := 0, len(tokens)
+			for start < end && tokens[start].Type == hclsyntax.TokenNewline {
+				start++
+			}
+			for end > start && tokens[end-1].Type == hclsyntax.TokenNewline {
+				end--
+			}
+			body.AppendUnstructuredTokens(tokens[start:end])
+			if i+1 < len(providerNames) {
+				body.AppendNewline()
+			}
+		}
+		body.AppendNewline()
+	}
 }
 
 // ProcessAndSortBlocks extracts sortable blocks (variables, outputs) from the HCL file
